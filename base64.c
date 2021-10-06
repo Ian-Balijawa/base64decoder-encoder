891ff155f88e267b98e2a5d2ee0bd1ebf0fbb3ee
ian balijawa <ianbalijawa16@gmail.com>
2021-10-07 02:31:21 +0300

    implement the base64_encoding sub routine

--- base64.c
+++ base64.c
@@ -26,4 +26,34 @@ void build_decoding_table() {
 void base64_cleanup() {
     free(decoding_table);
 } 
+
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length) {
+ 
+    *output_length = 4 * ((input_length + 2) / 3);
+ 
+    char *encoded_data = malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+ 
+    for (int i = 0, j = 0; i < input_length;) {
+ 
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+ 
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+ 
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+ 
+    for (int i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+ 
+    return encoded_data;
+}
+ 
  
